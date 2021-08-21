15e28c7aff8d396eb933b8141eef2c923233fb33
Stanislav Malík <stanislav.malik@student.tuke.sk>
2021-08-21 11:53:47 +0200

    add icons on home page

--- js/templates/home.js
+++ js/templates/home.js
@@ -13,7 +13,10 @@ class Home {
               <h1>${lang.title}</h1>
               <p>${lang.info}</p>
               <p>${lang.warning}</p>
-              <p><a class="link" href="#">Git</a></p>
+              <p class="iconContainer">
+                <a class="link" href="#"><i class="fab fa-lg fa-github"></i></a>
+                <a class="link" href="#"><i class="fab fa-lg fa-android"></i></a>
+              </p>
             </div>
         `;
     }
