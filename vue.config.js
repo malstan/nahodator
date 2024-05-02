c6ab049f3b51d02487ce4254754722db3de46335
Stanislav Malík <stanislav.malik@student.tuke.sk>
2024-05-02 21:38:57 +0200

    update icons and manifest for pwa

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  pwa: {
+    name: "Náhodator",
+    themeColor: "#0b6e11",
+    msTileColor: "#292524",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "#292524",
+    iconPaths: {
+      faviconSVG: "img/icons/favicon.svg",
+      favicon32: "img/icons/favicon-32x32.png",
+      favicon16: "img/icons/favicon-16x16.png",
+      appleTouchIcon: "img/icons/apple-touch-icon-152x152.png",
+      maskIcon: "img/icons/safari-pinned-tab.svg",
+      msTileImage: "img/icons/mstile-150x150.png",
+      androidChrome192: "img/icons/android-chrome-192x192.png",
+    },
+  },
+});
