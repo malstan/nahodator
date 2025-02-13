38fc49dd66baf6e0738e72f9adadeeb9183e6eff
Stanislav Malík <stanislavmalik@outlook.com>
2025-02-13 16:35:31 +0100

    update publicPath and add deployment workflow for Vue.js PWA

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/nahodator/" : "/",
+  publicPath: "./",
   transpileDependencies: true,
   pwa: {
     name: "Náhodator",
